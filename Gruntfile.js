5feb2b1cfd6f001ce2b1e731e3ceb1d16b8a40b0
James Washington <jbwashington@gmail.com>
2015-12-16 10:39:12 -0600

    added grunt configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,62 @@
 module.exports = function(grunt){
 
+  /** * Load Grunt plugins **/
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
   // intial grunt configuration
   grunt.initConfig({
 
     // load tasks from package.json
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
+    /**
+     * Project banner
+     */
+    tag: {
+      banner: '/*!\n' +
+        ' * <%= pkg.name %>\n' +
+          ' * <%= pkg.title %>\n' +
+            ' * <%= pkg.url %>\n' +
+              ' * @author <%= pkg.author %>\n' +
+                ' * @version <%= pkg.version %>\n' +
+                  ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+                    ' */\n'
+    },
+      sass: {
+        dist: {
+        files: {
+          'lib/css/main.css' : 'src/app/styles/main.scss'
       },
-      dist: {
-        src: ['src/scripts/**/*.js'],
-        dest: 'lib/js/**/.js'
+        },
+      /**
+       * Watch
+       */
+    watch: {
+      scripts: {
+      files: ['**/*.js'],
+      tasks: ['jshint'],
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
       }
     },
+      },
+   /**
+    * WireDep
+    */
     wiredep: {
-
       task: {
-
         // Point to the files that should be updated when
         // you run `grunt wiredep`
         src: [
-          '/src/**/*.html',   // .html support...
-          '/src/**/*.jade',   // .jade support...
-          '/src/styles/main.scss',  // .scss & .sass support...
-          '/src/config.yml'         // and .yml & .yaml support out of the box!
+          '/src/app/**/*.html',   // .html support...
+          '/src/app/**/*.jade',   // .jade support...
+          '/src/app/styles/main.scss',  // .scss & .sass support...
+          '/src/app/config.yml'         // and .yml & .yaml support out of the box!
         ],
         target: {
           src: '/lib/index.html' // point to HTML file
         },
-
         options: {
           // See wiredep's configuration documentation for the options
           // you may pass:
@@ -39,12 +66,6 @@ module.exports = function(grunt){
       }
     }
   });
-
-    grunt.loadNpmTasks('grunt-wiredep');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    // task setup
-    grunt.registerTask('default', ['grunt-wiredep', 'jshint', 'concat']);
-
-  };
+  // task setup
+  grunt.registerTask('default', ['sass', 'watch']);
+};
